78725c3ebd711c55785d587e6a5d7630fc37a4d2
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-05 12:52:21 +0600

    Added initial setups to the project

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-50 py-5">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
+        <h2 className="font-semibold text-lg text-gray-700">
+          Responsive Image Gallery
+        </h2>
+        <Link className="font-medium text-lg text-gray-700" href="/v2">
+          Checkout Version 02
+        </Link>
+      </div>
+    </nav>
+  );
+}
